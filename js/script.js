1f57846f482c120c1686f6ad7223167ecb92b7cc
FelixYale <felix.awoyale@hashtagng.com>
2023-07-24 01:59:37 +0100

    feat: function for transparent nav

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navLink.classList.toggle("active");
 });
+
+navHeader = document.querySelector(".nav");
+navA = document.querySelectorAll(".nav-a");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navHeader.classList.add("nav-scrolled");
+    for (const element of navA) {
+      element.classList.add("nav-a-scrolled");
+    }
+  } else if (window.scrollY <= 50) {
+    navHeader.classList.remove("nav-scrolled");
+    for (const element of navA) {
+      element.classList.remove("nav-a-scrolled");
+    }
+  }
+});
