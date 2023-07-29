8505b8d8a6fcc714b9f360f2631ebb5edf172ad9
FelixAwoyale <dexteryale@gmail.com>
2023-07-29 23:00:09 +0100

    feat: add onsubmit and email js function

--- js/script.js
+++ js/script.js
@@ -75,3 +75,79 @@ const ServicesStyleOne = new Swiper(".productswiper", {
 
 // dropDownShow = document.querySelector('.dropdown');
 // const
+
+// const ValidateMail = () => {
+//   if (params.email.match(validRegex)) {
+//     return true;
+//   } else {
+//     // alert("Invalid email address!");
+
+//     // console.log("wrong mail");
+
+//     return false;
+//   }
+// };
+
+const emailJs = (myparams) => {
+  const serviceID = "service_27psmqd";
+  const TemplateID = "template_mxah4ej";
+  emailjs
+    .send(serviceID, TemplateID, myparams)
+    .then((res) => {
+      (document.getElementById("name").value = ""),
+        (document.getElementById("email").value = ""),
+        (document.getElementById("phone").value = ""),
+        (document.getElementById("message").value = "");
+      console.log(res);
+      alert("your message was sent succesfully");
+    })
+    .catch((err) => console.log(err));
+};
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  var myparams = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    phone: document.getElementById("phone").value,
+    message: document.getElementById("message").value,
+  };
+  const serviceID = "service_27psmqd";
+  const TemplateID = "template_mxah4ej";
+  emailjs
+    .send(serviceID, TemplateID, myparams)
+    .then((res) => {
+      (document.getElementById("name").value = ""),
+        (document.getElementById("email").value = ""),
+        (document.getElementById("phone").value = ""),
+        (document.getElementById("message").value = "");
+      console.log(res);
+      alert("your message was sent succesfully");
+    })
+    .catch((err) => console.log(err));
+  // emailJs(params);
+});
+
+// function sendMail() {
+//   var params = {
+//     name: document.getElementById("name").value,
+//     email: document.getElementById("email").value,
+//     phone: document.getElementById("phone").value,
+//     message: document.getElementById("message").value,
+//   };
+//   var validRegex =
+//     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+//   if (params.email.match(validRegex)) {
+//     // var params = {
+//     //   name: document.getElementById("name").value,
+//     //   email: document.getElementById("email").value,
+//     //   phone: document.getElementById("phone").value,
+//     //   message: document.getElementById("message").value,
+//     // };
+//   } else {
+//     alert("wrong email address");
+//   }
+// }
